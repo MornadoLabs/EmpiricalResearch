3bc7c3311c0d495f7cba1cb41febbc0f3e6a2ed4
mornado <mornado12@mail.ru>
2018-09-11 00:41:52 +0300

    Lab1: Back-end done;

---
+++ Lab1/Lab1/Repositories/EmpericalSampleRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1.Repositories
+{
+    internal class EmpericalSampleRepository
+    {
+        private List<double> EmpericalSample = new List<double>
+        {
+            -5, -4.6, -4.6, -4.6, -4.4, -4.4, -4.4, -4.4, -4.2, -4.2, -4.2, -4.2, -4.2, -4, -3.8, -3.8, -3.6, -3.4, -3.2,
+            -3, -2.9, -2.7, -2.5, -2.5, -2.3, -2.1, -2.1, -2, -1.6, -1.6, -1.6, -1.4, -1.4, -1.4, -1.4, -1.2, -1.2, -1.2,
+            -1.2, -1.2, -1, -0.7, -0.7, -0.3, -0.3, 0, 0.3, 0.3, 0.7, 0.7, 1, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.4, 1.4, 1.4,
+            1.4, 1.6, 1.6, 1.6, 2, 2.1, 2.1, 2.3, 2.5, 2.7, 2.9, 3, 3.2, 3.4, 3.6, 3.8, 3.8, 4, 4.2, 4.2, 4.2, 4.2, 4.2,
+            4.4, 4.4, 4.4, 4.4, 4.6, 4.6, 4.6, 4.6, 4.6, 5,
+        };
+
+        public List<double> GetEmpericalSample()
+        {
+            return EmpericalSample;
+        }
+    }
+}
---
+++ Lab1/Lab1/Services/EmpericalSampleService.cs
@@ -0,0 +1,156 @@
+﻿using Lab1.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1.Services
+{
+    public class EmpericalSampleService
+    {
+        public EmpericalSampleService()
+        {
+            this.EmpericalSampleRepository = new EmpericalSampleRepository();        
+        }
+
+        private EmpericalSampleRepository EmpericalSampleRepository { get; set; }
+        
+
+        public Dictionary<double, int> GetDiscreteRowByFrequency()
+        {
+            var result = new Dictionary<double, int>();
+            var elements = this.GetSampleElements();
+
+            foreach (var element in elements)
+            {
+                result.Add(element, GetElementFrequency(element));
+            }
+
+            return result;
+        }
+
+        public Dictionary<double, double> GetDiscreteRowByVirtualFrequency()
+        {
+            var result = new Dictionary<double, double>();
+            var elements = this.GetSampleElements();
+
+            foreach (var element in elements)
+            {
+                result.Add(element, GetElementVirtualFrequency(element));
+            }
+
+            return result;
+        }
+
+        public double GetMiddleEmpericalValue()
+        {
+            return GetStartEmpericalMoment(1);
+        }
+
+        public double GetDeviation(double element)
+        {
+            var discreteRow = this.GetDiscreteRowByFrequency();
+            var middleEmpericalValue = this.GetMiddleEmpericalValue();
+            return (element - middleEmpericalValue) * discreteRow[element];
+        }
+
+        public List<double> GetMode()
+        {
+            var discreteRow = GetDiscreteRowByFrequency();
+            var modeValue = discreteRow.Values.Max();
+            return discreteRow.Where(pair => pair.Value == modeValue).Select(p => p.Key).ToList();
+        }
+
+        public double GetMedian()
+        {
+            var sample = EmpericalSampleRepository.GetEmpericalSample();
+            sample.Sort();
+
+            if (sample.Count % 2 == 0)
+            {
+                var middleId = sample.Count / 2;
+                return (sample[middleId] + sample[middleId - 1]) / 2;
+            }
+            else
+            {
+                return sample[sample.Count / 2];
+            }
+        }
+
+        public double GetDispersion()
+        {
+            return GetMiddleEmpericalMoment(2);
+        }
+
+        public double GetMiddleStandardDeviation()
+        {
+            return Math.Sqrt(GetDispersion());
+        }
+
+        public double GetSwing()
+        {
+            var elements = GetSampleElements();
+            return elements.Max() - elements.Min();
+        }
+
+        public double GetVariationCoef()
+        {
+            return GetMiddleStandardDeviation() / GetMiddleEmpericalValue() * 100;
+        }
+
+        public double GetStartEmpericalMoment(int k)
+        {
+            var discreteRow = this.GetDiscreteRowByFrequency();
+            var result = 0.0;
+
+            foreach (var element in discreteRow.Keys)
+            {
+                result += Math.Pow(element, k) * discreteRow[element];
+            }
+
+            return result / EmpericalSampleRepository.GetEmpericalSample().Count;
+        }
+
+        public double GetMiddleEmpericalMoment(int k)
+        {
+            var discreteRow = this.GetDiscreteRowByFrequency();
+            var result = 0.0;
+            var middleEmpericalValue = GetMiddleEmpericalValue();
+
+            foreach (var element in discreteRow.Keys)
+            {
+                result += Math.Pow(element - middleEmpericalValue, k) * discreteRow[element];
+            }
+
+            return result / EmpericalSampleRepository.GetEmpericalSample().Count;
+        }
+
+        public double GetAsymmetricalCoef()
+        {
+            return GetMiddleEmpericalMoment(3) / GetMiddleStandardDeviation();
+        }
+
+        public double GetExcess()
+        {
+            return GetMiddleEmpericalMoment(4) / Math.Pow(GetMiddleStandardDeviation(), 4) - 3;
+        }
+
+
+        private List<double> GetSampleElements()
+        {
+            return this.EmpericalSampleRepository.GetEmpericalSample().Distinct().ToList();
+        }
+
+        private int GetElementFrequency(double element)
+        {
+            return EmpericalSampleRepository.GetEmpericalSample().Count(elem => elem == element);
+        }
+
+        private double GetElementVirtualFrequency(double element)
+        {
+            return
+                EmpericalSampleRepository.GetEmpericalSample().Count(elem => elem == element) /
+                EmpericalSampleRepository.GetEmpericalSample().Count();
+        }
+    }
+}
